f46f427ffd103b8d64da6e804351381a548e0c12
Eugene <evgenii.esaulenko@gmail.com>
2025-04-27 15:40:56 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+﻿#include "raylib-cpp.hpp"
+
+int main()
+{
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 450;
+
+    raylib::Window window(screenWidth, screenHeight, "Dino");
+    window.SetState(FLAG_VSYNC_HINT);
+    window.SetTargetFPS(60);
+
+    // Load game
+
+    while (!WindowShouldClose())
+    {
+        // Update game
+
+        // Draw game
+        window.BeginDrawing();
+        window.ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        window.EndDrawing();
+    }
+
+    // Unload game
+
+    return 0;
+}
